549b99cda59768d5c1ca405059890b6e74e97407
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-04-16 10:30:05 -0500

    add comments to apiroutes and htmlroutes files

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,4 @@
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
+//Establishes the api routes for getting and posting data to the db.json file
 
 const noteData = require('../db/db');
 const generateUniqueId = require('generate-unique-id');
@@ -14,27 +12,15 @@ const id = generateUniqueId({
 // ROUTING
 
 module.exports = (app) => {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
+ 
 
   app.get('/api/notes', (req, res) => res.json(noteData));
 
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
+  
 
   app.post('/api/notes', (req, res) => {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
+    
     
       noteData.push(req.body);
       res.json(true);
