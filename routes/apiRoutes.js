680713862e1612894f2b6c2329c77f8df8907223
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-04-18 08:47:09 -0500

    clean commit to main

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -41,7 +41,6 @@ module.exports = (app) => {
     res.json(true)
   ; 
 
-<<<<<<< HEAD
   res.json(true); 
   });
 
@@ -49,25 +48,4 @@ module.exports = (app) => {
   //   console.log(req.params.id);
   // })
 
-=======
-  app.delete('/api/notes:id', (req, res) => {  
-    console.log(req.param.id);
-    // let allNotes = [];
-
-    // fs.readFile('./db/db.json', (err, data) => {
-    //   if(err) {
-    //     throw(err)
-    //   } else {
-    //     allNotes = JSON.parse(data);
-    //     deletedData.splice(key, 1);
-    //     res.send(deletedData);
-    //   }
-    // })
-  
-  })
-});
-
-  
-
->>>>>>> 39dc9ffe9e80137679d55b789c66cb66a9185548
 };
