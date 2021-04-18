895f13210badaefb5d408854835d3a541c824eda
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-04-18 11:00:04 -0500

    change * to / in index.html route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -10,7 +10,7 @@ module.exports = (app) => {
     });;
   
     
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
       res.sendFile(path.join(__dirname, '../public/index.html'));
     });
   };
